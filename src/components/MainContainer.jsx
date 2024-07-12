636c9b2b29b735115e2b0d84855cf10f6e1f61ee
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2024-07-13 00:07:59 +0530

    bug: data is static

---
+++ src/components/MainContainer.jsx
@@ -0,0 +1,16 @@
+// import useMovies from "../hooks/useMovies"
+import { BackgroundVideo, Title } from "./index"
+
+const MainContainer = () => {
+
+  // useMovies();
+ 
+  return (
+    <div className="">
+      <Title />
+      <BackgroundVideo />
+
+    </div>
+  )
+}
+export default MainContainer
