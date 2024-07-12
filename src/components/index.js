1ad97555ac61d6107dc54a5afbc9a1d5ff8bde1c
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2024-07-13 00:03:58 +0530

    all files and constant data are located here

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,23 @@
+import BackgroundVideo from "./BackgroundVideo";
 import Body from "./Body";
 import Browse from "./Browse";
 import Header from "./Header";
 import Login from "./Login";
+import MainContainer from "./MainContainer";
+import SecondryContainer from "./SecondryContainer";
+import Title from "./Title";
+import useMovie from "../hooks/useMovies";
+
 
 export {
+    BackgroundVideo,
     Body,
     Browse,
     Header, 
     Login,
+    MainContainer,
+    SecondryContainer,
+    Title,
+    useMovie,
+
 }
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,5 @@
 export const AVTAR_URL = "https://avatars.githubusercontent.com/u/94777700?s=96&v=4";
+
+export const Netflix_RAPID_API_KEY = 'f7328ec4demsh5b3f481e92b4fabp193cbcjsn7a5174773597';
+
+export const Netflix_RAPID_API = 'https://streaming-availability.p.rapidapi.com/shows/%7Btype%7D/%7Bid%7D';
